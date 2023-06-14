a08ba7cbc9cc03177b3422bbbe7ac45426d90c4b
Julia Lin <36869576+julx23@users.noreply.github.com>
2023-06-14 18:47:50 +0100

    Update diff_w.m

--- diff_w.m
+++ diff_w.m
@@ -1,6 +1,5 @@
+% differentiates with a window w to reduce noise
 function out=diff_w(input,w)
-%     shift = [zeros(1,w) input];
-%     out = -shift(w:end)+input(w:end);
     out = (input(w:end)-input(1:end-w+1))/w;
     out = [zeros(1,w) out];
 end
